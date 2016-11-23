ee4c78b0354f18d67ffc774e8affd660fc959117
thafsouth96 <thafsuth.adli@gmail.com>
2016-11-23 15:38:05 +0100

    still working :(

--- src/EasyScrum/EasyScrumBundle/Resources/public/js/script.js
+++ src/EasyScrum/EasyScrumBundle/Resources/public/js/script.js
@@ -9,13 +9,30 @@ $(document).ready(function(){
 
   $(function(){
    $(window).scroll(function () {//Au scroll dans la fenetre on déclenche la fonction
-      if ($(this).scrollTop() > 80) { //si on a défilé de plus de 150px du haut vers le bas
+      if ($(this).scrollTop() > 80) { //si on a défilé de plus de 80px du haut vers le bas
           $('#navigation').addClass("fixNavigation"); //on ajoute la classe "fixNavigation" à <div id="navigation">
       } else {
       $('#navigation').removeClass("fixNavigation");//sinon on retire la classe "fixNavigation" à <div id="navigation">
       }
    });
  });
+
+ /* Modification de la page de connexion */
+/*var divUserOpen = $('<div id="userDiv">') ;
+var divPassword = $('<div id="PasswordDiv">');
+var divRemember = $('<div id="rememberDiv">');
+var divClosed = $('</div>');
+
+$('label[for=username]').prepend(divUserOpen);
+$('#username').append(divClosed) ;
+
+$('label[for=password]').prepend(divPassword);
+$('#password').append(divClosed);
+
+$('#remember_me').prepend(divRemember);
+$('label[for="remember_me"]').append(divClosed);
+
+*/
 
 
 });
